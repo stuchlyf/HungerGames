057a5a864fd3889872578501fdb66054dcf3be35
Florian Stuchly <florian.stuchly@gmail.com>
2022-04-13 17:52:30 +0200

    Initial Commit

---
+++ hunger-games-backend/src/test/java/de/stuchlyf/hungergamesbackend/HungerGamesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.stuchlyf.hungergamesbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HungerGamesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
